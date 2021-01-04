7d03906675af73d67eceaca884f44cde6b9b0397
Edgar Manukyan <armenic@gmail.com>
2021-01-03 23:10:27 -0500

    reach 5.5.2 ex 2

---
+++ README.md
@@ -0,0 +1,5 @@
+# Reviewing https://r4ds.had.co.nz
+
+## Current Chapter 
+### Ch 5
+https://r4ds.had.co.nz/transform.html#exercises-11
--- ch5.R
+++ ch5.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(nycflights13)
+library(stringr)
 
 
 # Had an arrival delay of two or more hours
@@ -50,7 +51,66 @@ View(zz)
 
 
 
+# How could you use arrange() to sort all missing values to the start? (Hint:
+# use is.na()).
+df <- tibble(x = c(5, NA, 2, NA))
+arrange(df, desc(is.na(x)))
 
+
+# Sort flights to find the most delayed flights. Find the flights that left
+# earliest.
+flights %>% 
+  arrange(desc(dep_delay))
+flights %>% 
+  arrange(dep_delay)
+
+
+# Sort flights to find the fastest (highest speed) flights.
+zz <- flights %>% 
+  filter(!is.na(distance) & !is.na(air_time)) %>% 
+  mutate(speed = distance / (air_time / 60)) %>% 
+  arrange(desc(speed))
+View(zz)
+
+# Which flights travelled the farthest? Which travelled the shortest?
+flights %>% 
+  arrange(desc(distance))
+flights %>% 
+  arrange(distance)
+
+
+flights %>% 
+  select(any_of(c("year", "month", "day", "dep_delay", "arr_delay")))
+
+select(flights, contains("TIME", ignore.case = FALSE))
+
+transmute(flights,
+          gain = dep_delay - arr_delay,
+          hours = air_time / 60,
+          gain_per_hour = gain / hours
+)
+
+
+# Currently dep_time and sched_dep_time are convenient to look at, but hard to
+# compute with because they’re not really continuous numbers. Convert them to a
+# more convenient representation of number of minutes since midnight.
+
+zz <- flights %>%
+  select(dep_time, sched_dep_time) %>%
+  mutate(
+    dep_time_str = str_pad(dep_time, 4, "left", "0"),
+    sched_dep_time_str = str_pad(sched_dep_time, 4, "left", "0"),
+    dep_time_num = as.numeric(substr(dep_time_str, 1, 2)) * 60 +
+      as.numeric(substr(dep_time_str, 3, 4)),
+    sched_dep_time_num = as.numeric(substr(sched_dep_time_str, 1, 2)) * 60 +
+      as.numeric(substr(sched_dep_time_str, 3, 4))
+  ) %>% 
+  select(!c(dep_time_str, sched_dep_time_str))
+View(zz)
+
+
+# Compare air_time with arr_time - dep_time. What do you expect to see? What do
+# you see? What do you need to do to fix it?
 
 
 
