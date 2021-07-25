590513bdc898b923e6e0accc327437d22023d0c1
Edgar Manukyan <edgar.manukyan@roche.com>
2021-07-25 17:10:28 +0000

    done with ch7

--- R/ch7.R
+++ R/ch7.R
@@ -164,8 +164,109 @@ ggplot(diamonds) +
 # categorical -------------------------------------------------------------
 
 
+# To visualise the covariation between categorical variables, you'll need to 
+# count the number of observations for each combination. 
+# Covariation will appear as a strong correlation between specific x values and
+# specific y values.
+ggplot(diamonds) + 
+  geom_count(aes(x = cut, y = color))
+
+counts <- diamonds %>% 
+  count(color, cut) %>% 
+  print()
+
+counts %>% 
+  ggplot(aes(x = color, y = cut)) + 
+  geom_tile(aes(fill = n))
 
 
+# exercises ---------------------------------------------------------------
+
+# How could you rescale the count dataset above to more clearly show the
+# distribution of cut within colour, or colour within cut?
+counts <- diamonds %>% 
+  count(color, cut) %>% 
+  mutate(n = n * 0.001) %>% 
+  print()
+counts %>% 
+  ggplot(aes(x = color, y = cut)) + 
+  geom_tile(aes(fill = n))
 
 
+# continuous --------------------------------------------------------------
+ggplot(diamonds) + 
+  geom_point(aes(x = carat, y = price), alpha = 1 / 10)
 
+
+ggplot(diamonds) + 
+  geom_bin2d(aes(x = carat, y = price))
+
+smaller <- diamonds %>% 
+  filter(carat < 3)
+
+
+ggplot(smaller, aes(x = carat, y = price)) + 
+  geom_boxplot(aes(group = cut_width(carat, 0.1)))
+
+# exercises ---------------------------------------------------------------
+
+
+# Instead of summarising the conditional distribution with a boxplot, you could
+# use a frequency polygon. What do you need to consider when using cut_width()
+# vs cut_number()? How does that impact a visualisation of the 2d distribution
+# of carat and price?
+
+smaller <- diamonds %>% 
+  filter(carat < 3)
+
+ggplot(smaller, aes(x = price, color = cut_number(carat, 5))) + 
+  geom_freqpoly(binwidth = 5)
+
+# Visualise the distribution of carat, partitioned by price.
+ggplot(smaller, aes(x = price, y = carat)) + 
+  geom_boxplot(aes(group = cut_width(price, 1000)))
+
+
+# How does the price distribution of very large diamonds compare to small
+# diamonds? Is it as you expect, or does it surprise you?
+# it seems that large diamonds have wider distributions compared with small 
+# diamonds.
+
+
+# Patterns provide one of the most useful tools for data scientists because they
+# reveal covariation. If you think of variation as a phenomenon that creates
+# uncertainty, covariation is a phenomenon that reduces it. If two variables
+# covary, you can use the values of one variable to make better predictions
+# about the values of the second. If the covariation is due to a causal
+# relationship, then you can use the value of one variable to control the value
+# of the second.
+ggplot(data = faithful) + 
+  geom_point(mapping = aes(x = eruptions, y = waiting))
+
+# Models are a tool for extracting patterns out of data. For example, consider
+# the diamonds data. It's hard to understand the relationship between cut and
+# price, because cut and carat, and carat and price are tightly related. It's
+# possible to use a model to remove the very strong relationship between price
+# and carat so we can explore the subtleties that remain. The following code
+# fits a model that predicts price from carat and then computes the residuals
+# (the difference between the predicted value and the actual value). The
+# residuals give us a view of the price of the diamond, once the effect of carat
+# has been removed.
+
+library(modelr)
+
+mod <- lm(log(price) ~ log(carat), data = diamonds)
+
+diamonds2 <- diamonds %>% 
+  add_residuals(mod) %>% 
+  mutate(resid = exp(resid))
+
+ggplot(diamonds2) + 
+  geom_point(aes(x = carat, y = resid))
+
+# Once you've removed the strong relationship between carat and price, you can 
+# see what you expect in the relationship between cut and price: relative to 
+# their size, better quality diamonds are more expensive
+
+ggplot(diamonds2) +
+  geom_boxplot(aes(x = cut, y = resid))
