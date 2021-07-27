dac69cf2d1ca0c6ac3c1853ac2eb3e84c13ad6ef
Edgar Manukyan <edgar.manukyan@roche.com>
2021-07-27 13:26:43 +0000

    add ch15

---
+++ R/ch15.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+
+
+x1 <- c("Dec", "Apr", "Jan", "Mar")
+
+month_levels <- c(
+  "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+)
+
+y1 <- factor(x1, levels = month_levels)
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Reviewing https://r4ds.had.co.nz
 
 ## Current Chapter 
-### Ch 15
-https://r4ds.had.co.nz/factors.html
+### Ch 16
+https://r4ds.had.co.nz/dates-and-times.html
